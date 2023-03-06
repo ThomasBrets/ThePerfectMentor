1acec9bb3ab4e4b4a98b9fb2e8f4c7cb67d55cd1
IsaacManzo <isaacemanzo51@gmail.com>
2023-03-06 13:05:17 -0300

    Hola

---
+++ src/front/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./components/Home.jsx";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/front/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../componentsCss/Home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Fala muito, chupa piroca do Messi</h1>
+        <img className="messi" src="https://eleditorplatense.com.ar/wp-content/uploads/2023/01/Lionel-Messi-Copa-del-Mundo.jpg" alt="Foto de messi" />
+    </div>
+  )
+}
+
+export default Home
