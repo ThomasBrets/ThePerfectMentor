31518db47b61890c0e249f57131f393c89c867dd
ThomasBrets <thomas15501331@gmail.com>
2023-03-20 12:56:51 -0300

    delete comments

--- api/services/authServices.js
+++ api/services/authServices.js
@@ -1,45 +1,6 @@
 const User = require("../models/Users");
 const joi = require("../config/joi");
 
-// class AuthService {
-//   static async createUser({
-//     name,
-//     lastName,
-//     email,
-//     age,
-//     password,
-//     areaCode,
-//     phoneNumber,
-//     country,
-//     photo,
-//   }) {
-//     try {
-//       const { error, value } = joi.validate({ name, password, email });
-
-//       if(!error) {
-//         const user = new User({
-//           name,
-//           lastName,
-//           email,
-//           age,
-//           password,
-//           areaCode,
-//           phoneNumber,
-//           country,
-//           photo,
-//         })
-//         console.log("USER", user);
-//         const resp = await user.save
-
-//         return { error: false, data: resp };
-//       }
-//       return { error: true, data: error };
-//     } catch (error) {
-//       return { error: true, data: error.message };
-//     }
-//   }
-// }
-
 class AuthService {
   static async createUser({
     name,
