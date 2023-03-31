43f239be60e7df038cd9da45977678d9aa8e7f3e
IsaacManzo <isaacemanzo51@gmail.com>
2023-03-31 19:42:49 -0300

    arreglos

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,24 +1,17 @@
-const express = require("express");
 const User = require("../models/Users");
-const mongoose = require("mongoose");
 const AuthService = require("../services/auth");
 const { generateToken, validateToken } = require("../config/tokens");
-// const {validatePassword} = require("../models/Users")
 
 class AuthController {
   static async createUser(req, res) {
     const { error, data } = await AuthService.createUser(req.body);
-    // console.log("DATA", data);
     return error
       ? res.status(data.status || 500).json({ message: data })
       : res.status(201).json(data);
   }
   static async loginUser(req, res) {
     const { email, password } = req.body;
-    console.log("EMAIL", email);
-    console.log("PASSWORD", password);
-
-      User.findOne({ email })
+    User.findOne({ email })
       .then((user) => {
         console.log("USER", user);
         if (!user) return res.sendStatus(401);
@@ -36,10 +29,9 @@ class AuthController {
 
           res.send(payload);
         });
-      }).catch((error) => console.log(error))
-      
+      })
+      .catch((error) => console.log(error));
   }
- 
 }
 
 module.exports = AuthController;
