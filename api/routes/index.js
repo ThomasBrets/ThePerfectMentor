cb7187659159bcd2c9404915c6184465b2f6b182
IsaacManzo <isaacemanzo51@gmail.com>
2023-04-20 11:27:11 -0300

    ahora si

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,21 +6,13 @@ const {validateUser, checkAuthAdmin} = require("../middleware/auth")
 const auth = require("./auth")
 const users = require("./users")
 const admin = require("./admin")
-<<<<<<< HEAD
 const mentor = require("./mentor")
-=======
-const post = require("./posts")
 const match = require("./match")
->>>>>>> c7d8a31b5fc51910614cb162824da3507be46f1c
 
 router.use("/auth", auth);
 router.use("/users", validateUser, users);
 router.use("/admin", checkAuthAdmin, admin)
-<<<<<<< HEAD
 router.use("/mentor", mentor)
-=======
-router.use("/posts", post)
 router.use("/match", match)
->>>>>>> c7d8a31b5fc51910614cb162824da3507be46f1c
 
 module.exports = router
