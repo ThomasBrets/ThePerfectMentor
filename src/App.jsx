ad5bff1da4f89b4a87c69b624cd0bec01341eeb3
IsaacManzo <isaacemanzo51@gmail.com>
2023-05-10 12:42:01 -0300

    yup en form

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import UserPage from "./commons/UserPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -24,7 +24,7 @@ function App() {
         <Route path="/user-page" element={<UserPage />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+      </div>
   );
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("El nombre es obligatorio"),
+  lastName: Yup.string().required("El apellido es obligatorio"),
+  email: Yup.string()
+    .email("Correo electrónico inválido")
+    .required("El correo electrónico es obligatorio"),
+  password: Yup.string()
+    .required("La contraseña es requerida")
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d).{8,}$/,
+      "La contraseña debe contener al menos una mayúscula, un número y tener un mínimo de 8 caracteres"
+    ),
+  mentor: Yup.boolean(),
+});
 
 const Register = () => {
   const formik = useFormik({
@@ -11,6 +27,7 @@ const Register = () => {
       password: "",
       mentor: false,
     },
+    validationSchema,
     onSubmit: (values) => {
       axios
         .post("http://localhost:3001/api/auth/register", values)
@@ -52,10 +69,17 @@ const Register = () => {
                   type="text"
                   name="name"
                   id="name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className={`bg-gray-50 border ${
+                    formik.errors.name ? "border-red-500" : "border-gray-300"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                   onChange={formik.handleChange}
                   value={formik.values.name}
                 />
+                {formik.errors.name && (
+                  <div className="text-red-500 text-xs mt-1">
+                    {formik.errors.name}
+                  </div>
+                )}
               </div>
               <div>
                 <label
@@ -71,8 +95,12 @@ const Register = () => {
                   id="lastName"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   value={formik.values.lastName}
                 />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <div className="text-red-500">{formik.errors.lastName}</div>
+                ) : null}
               </div>
               <div>
                 <label
@@ -88,8 +116,12 @@ const Register = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="nombre@gmail.com"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   value={formik.values.email}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="text-red-500">{formik.errors.email}</div>
+                ) : null}
               </div>
               <div>
                 <label
@@ -105,8 +137,12 @@ const Register = () => {
                   id="password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   value={formik.values.password}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-red-500">{formik.errors.password}</div>
+                ) : null}
               </div>
               <div>
                 <label
