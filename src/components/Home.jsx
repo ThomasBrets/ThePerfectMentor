1c93e4037dd0abf5c7799635760edc5d58784f40
IsaacManzo <isaacemanzo51@gmail.com>
2023-07-14 23:06:56 -0300

    reinicio front

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-
-import Card from "../commons/Card";
 import Footer from "./Footer";
 
 const Home = ({ user, mentor }) => {
   return (
     <div>
-      <Card mentor={mentor} />
       <Footer />
     </div>
   );
