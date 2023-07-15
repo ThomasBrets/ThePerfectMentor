f350ba6634816904e26e4fd835f36cd345b5733c
ThomasBrets <thomas15501331@gmail.com>
2023-07-15 01:49:48 -0300

    flowbite2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,9 @@ import Card from "../commons/Card";
 
 const Home = ({ user, mentor }) => {
   return (
-    <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
-    <div class="h-32 rounded-lg">
+    <div>
       <Card mentor={mentor} />
     </div>
-
-
-  </div>
-
   );
 };
 
